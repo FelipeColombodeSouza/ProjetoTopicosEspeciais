92b2b1ee00ed4e7aa35a5c8f2335759e3460b9a9
Vinicius de Lima Xavier <53457191+ViniciusDLx@users.noreply.github.com>
2024-04-22 14:49:22 -0300

    FEAT(servicos): Criado o Inserir Atendimento no ServAtendimento

---
+++ Servicos/DTO/InserirAtendimentoDTO.cs
@@ -0,0 +1,17 @@
+﻿using Entidades;
+
+namespace Servicos
+{
+    public class InserirAtendimentoDTO
+    {
+        public string? Descricao { get; set; }
+
+        public decimal Valor { get; set; }
+
+        public DateTime DataConsulta { get; set; }
+
+        public int CodigoPaciente { get; set; }
+
+        public int CodigoProfissional { get; set; }
+    }
+}
---
+++ Servicos/ServAtendimento.cs
@@ -0,0 +1,33 @@
+﻿using Entidades;
+using Repositorio;
+
+namespace Servicos
+{
+    public interface IServAtendimento
+    {
+        void Inserir(InserirAtendimentoDTO inserirAtendimentoDto);
+    }
+
+    public class ServAtendimento : IServAtendimento
+    {
+        private IRepoAtendimento _repoAtendimento;
+
+        public ServAtendimento(IRepoAtendimento repoAtendimento)
+        {
+            _repoAtendimento = repoAtendimento;
+        }
+
+        public void Inserir(InserirAtendimentoDTO inserirAtendimentoDto)
+        {
+            var atendimento = new Atendimento();
+
+            atendimento.Descricao = inserirAtendimentoDto?.Descricao;
+            atendimento.Valor = inserirAtendimentoDto.Valor;
+            atendimento.DataConsulta = inserirAtendimentoDto.DataConsulta;
+            atendimento.CodigoPaciente = inserirAtendimentoDto.CodigoPaciente;
+            atendimento.CodigoProfissional = inserirAtendimentoDto.CodigoProfissional;
+
+            _repoAtendimento.Inserir(atendimento);
+        }
+    }
+}
